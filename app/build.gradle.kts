9d58938dbeb40b78423cbae60de3ef3fd0e61758
stipe-jurkovic <sjurko00@fesb.hr>
2025-11-08 20:47:25 +0100

    add icon files to git

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.iksica.myapplication"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
